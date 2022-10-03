842784ac17529cd321b49eec57569fda40b9f85a
Chanwoo Jung <jungcali94@gmail.com>
2022-10-03 16:32:00 +0900

    글쓰기 기능 및 vue 화면 추가

---
+++ front/src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import axios from "axios";
+import {ref} from "vue";
+
+const posts = ref([]);
+
+const page = ref("")
+const size = ref("")
+
+axios.get("/api/posts?page=1&size=10").then((response) => {
+  response.data.forEach((r: any) => {
+    posts.value.push(r);
+  })
+})
+</script>
+
+<template>
+  <ul>
+    <li v-for="post in posts" :key="post.id">
+      <div>
+        {{ post.title }}
+      </div>
+      <div>
+        {{ post.content }}
+      </div>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+li {
+ margin-bottom: 1rem;
+}
+
+li:last-child {
+  margin-bottom: 0;
+}
+</style>
--- front/src/views/WriteView.vue
+++ front/src/views/WriteView.vue
@@ -1,12 +1,39 @@
-<template>
-$END$
-</template>
+<script setup lang="ts">
+import {ref} from "vue";
 
-<script>
-export default {
-name: "WriteView"
+import axios from "axios";
+import { useRouter } from "vue-router";
+
+const title = ref("")
+const content = ref("")
+
+const router = useRouter();
+
+const write = function () {
+  axios
+    .post("/api/posts", {
+      title: title.value,
+      content: content.value
+    })
+    .then(() => {
+      router.replace({name: "home"})
+    })
 }
 </script>
+
+<template>
+  <div>
+    <el-input v-model="title" placeholder="제목을 입력해주세요" />
+  </div>
+
+  <div class="mt-2">
+    <el-input v-model="content" type="textarea" rows="15" />
+  </div>
+
+  <div class="mt-2">
+    <el-button type="primary" @click="write()">글 작성</el-button>
+  </div>
+</template>
 
 <style scoped>
 
